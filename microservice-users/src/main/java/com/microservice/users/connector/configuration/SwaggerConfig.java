8466c9d7832d0503a2bc249bbaad6833d901f5ee
Christian Peralta <christianipc.1987@gmail.com>
2025-05-08 12:32:34 -0300

    #5 documentación del ms de usuarios, elimino codigo innecesario

--- microservice-users/src/main/java/com/microservice/users/connector/configuration/SwaggerConfig.java
+++ microservice-users/src/main/java/com/microservice/users/connector/configuration/SwaggerConfig.java
@@ -1,19 +1,14 @@
 package com.microservice.users.connector.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
-        //final String securitySchemeName = "Bearer Token";
 
         return new OpenAPI()
                 .info(
@@ -21,17 +16,6 @@ public class SwaggerConfig {
                                 .description("API documentación de las distintas funcionalidades del microservicio de usuarios ")
                                 .version("1.0")
                 );
-                /*.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .tags(List.of(
-                        new Tag().name("API de gestión de autenticación y usuarios").description("Autenticación y usuarios")
-                ))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));*/
     }
 
 
