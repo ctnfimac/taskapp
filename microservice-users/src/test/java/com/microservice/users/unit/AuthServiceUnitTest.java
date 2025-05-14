b1b380bb2e1df317999ec332179bfdc8deea2d00
Christian Peralta <christianipc.1987@gmail.com>
2025-05-14 17:13:09 -0300

    #19 MS user, agrego test unitario para el login

--- microservice-users/src/test/java/com/microservice/users/unit/AuthServiceUnitTest.java
+++ microservice-users/src/test/java/com/microservice/users/unit/AuthServiceUnitTest.java
@@ -3,9 +3,12 @@ package com.microservice.users.unit;
 import com.microservice.users.entities.UserEntity;
 import com.microservice.users.repositories.UserRepository;
 import com.microservice.users.services.AuthServiceImpl;
+import net.bytebuddy.asm.Advice;
+import org.apache.catalina.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mapstruct.control.MappingControl;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -32,7 +35,7 @@ public class AuthServiceUnitTest {
 
     @Test
     @DisplayName("Test para verificar el registro exitoso de un usuario")
-    void registerUser_success(){
+    void register_should_saveuserEntity(){
         // given
         UserEntity user = UserEntity.builder()
                         .email("test@gmail.com")
@@ -61,6 +64,41 @@ public class AuthServiceUnitTest {
         verify(userRepository, Mockito.atMostOnce()).save(user);
         assertAll(() -> assertNotNull(result),
                 () -> assertEquals(result.getEmail(), user.getEmail())
+        );
+    }
+
+    @Test
+    @DisplayName("Test para verificar que existe un user con el valor de email y password")
+    void login_should_return_userentity(){
+        // given
+        UserEntity userLogin = UserEntity.builder()
+                .email("christian@gmail.com")
+                .password("123456")
+                .build();
+
+        LocalDateTime dateFake = LocalDateTime.now();
+
+        UserEntity userSearched = UserEntity.builder()
+                .id(2L)
+                .email("christian@gmail.com")
+                .password("123456")
+                .createdAt(dateFake)
+                .updatedAt(dateFake)
+                .build();
+
+        Mockito.when(userRepository.getUserEntityByEmailAndPassword(
+                "christian@gmail.com", "123456"
+        )).thenReturn(userSearched);
+
+        // when
+        UserEntity user = authService.login(userLogin);
+
+        // then
+        verify(userRepository, Mockito.atMostOnce()).getUserEntityByEmailAndPassword("christian@gmail.com", "123456");
+        assertAll(
+                () -> assertNotNull(user),
+                () -> assertEquals(user.getEmail(),userSearched.getEmail()),
+                () -> assertEquals(user.getId(),userSearched.getId())
         );
     }
 }
